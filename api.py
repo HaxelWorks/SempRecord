462367244d107d596439a47a56ddd02bf9e7d585
Yordi Verbeeck <hello@yordi.net>
2023-01-29 00:47:32 +0100

    Host frontend from frontend/build

--- api.py
+++ api.py
@@ -4,15 +4,20 @@ from flask_restful import Resource, Api
 import tray
 import recorder
 
-INDEX_PATH = r"frontend\public"
+INDEX_PATH = "frontend/public"
 # this path contains index.html and all the other frontend files
-app = Flask(__name__)
+app = Flask(__name__, static_folder = INDEX_PATH)
 api = Api(app)
 
 #serve index.html at the root
 @app.route("/")
 def index():
     return app.send_static_file("index.html")
+
+#serve build files too
+@app.route("/<path:path>")
+def static_proxy(path):
+    return app.send_static_file(path)
 
 
 @app.route("/api/status")

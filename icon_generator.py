9968e4ba0944ccd8cbfb7d1587eae99938cf863f
Axel Roijers <41266240+Pixel-Therapy@users.noreply.github.com>
2023-01-31 17:40:55 +0100

    Icon generator

--- icon_generator.py
+++ icon_generator.py
@@ -1,37 +1,57 @@
 from PIL import Image, ImageDraw
 
 SIZE = 32
-BLACK = (0, 0, 0, 0)
-RED = (255, 0, 0, 255)
+UPSAMPLE = 4
+TRANSP = (0, 0, 0, 0)
+RED = (245, 3, 1, 255)
 YELLOW = (255, 255, 0, 255)
 GREEN = (0, 255, 0, 255)
 BLUE = (0, 0, 255, 255)
 GRAY = (64, 64, 64, 255)
-def solid_circle(size: int, color: tuple, margin: int = 2):
-    image = Image.new("RGBA", size=(size, size), color=BLACK)
-    dc = ImageDraw.Draw(image)
-    # draw a red circle
-    delta = size - margin
-    dc.ellipse((margin, margin, delta, delta), fill=color)
-    return image
 
-def ring_circle(size: int, color: tuple, margin: int = 2, width: int = 4):
-    image = Image.new("RGBA", size=(size, size), color=BLACK)
-    dc = ImageDraw.Draw(image)
-    # draw a red circle
-    delta = size - margin
-    dc.ellipse((margin, margin, delta, delta), fill=color)
-    # draw a black circle
-    delta = size - margin - width
-    dc.ellipse((margin + width, margin + width, delta, delta), fill=BLACK)
+def icoratio(size: int):
+    sizes = [285, 247, 206, 56]
+    ratio = 256 / size
+    factor = 256 / 285
+    sizes = [int((size/ratio) * factor) for size in sizes]
+    return sizes
+
+
+def tray_icon_generator(size:int, color:tuple):
+    """Generates a tray icon using the cicle diameters from icoratio"""
+    size = size * UPSAMPLE
+    image = Image.new("RGBA", size=(size, size), color=TRANSP)
+    draw = ImageDraw.Draw(image)
+    d1,d2,d3,d4 =icoratio(size)
+    # draw a circle with diameter d1
+    delta = (size-d1)/2
+    draw.ellipse((delta, delta, size-delta-1, size-delta-1), fill=color)
+    # cut out a circle with diameter d2 from the center aka make it transparent
+    r1 = d1/2
+    r2 = d2/2
+    draw.ellipse((r1-r2, r1-r2, r1+r2, r1+r2), fill=TRANSP)
+    # fill with a circle with diameter d3
+    r3 = d3/2
+    draw.ellipse((r1-r3, r1-r3, r1+r3, r1+r3), fill=color)
+    # cut out a circle with diameter d4 from the center aka make it transparent
+    r4 = d4/2
+    draw.ellipse((r1-r4, r1-r4, r1+r4, r1+r4), fill=TRANSP)
+    
+    # downsample the image
+    image = image.resize((size//UPSAMPLE, size//UPSAMPLE), resample=Image.LANCZOS)
     return image
+    
+    
+
+    
+    
 
 class ICONS:
-    active = solid_circle(SIZE, RED)
-    standby = ring_circle(SIZE, RED, width=4)
-    paused = solid_circle(SIZE, YELLOW)
-    inactive = solid_circle(SIZE, GRAY)
-
+    active = tray_icon_generator(SIZE, RED)
+    paused = tray_icon_generator(SIZE, YELLOW)
+    standby = tray_icon_generator(SIZE, BLUE)
+    inactive = tray_icon_generator(SIZE, GRAY)
+    
 if __name__ == "__main__":
     # use pil to show the icons
     ICONS.active.show()
